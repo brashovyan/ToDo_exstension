5c7062163727804f2319a4755bab6497a58f79ac
Brashovyan Artem <artembrash@mail.ru>
2023-05-27 18:33:02 +0500

    логин, регистрация, список задач

---
+++ src/entry/background.js
@@ -0,0 +1,120 @@
+// При установке отправляю уведомление с инфой об расширении
+chrome.runtime.onInstalled.addListener(() => {
+    var options = {
+        title: "Спасибо за установку ToDO!",
+        message: "Тут короче описание",
+        iconUrl: "icons/128.png",
+        type: "basic",
+      };
+      chrome.notifications.create("", options);
+  });
+
+// запускаю таймер, который периодично будет вызывать функцию для проверки задач
+setInterval(check_tasks, 20000);
+
+function check_tasks(){
+    // посылаю post запрос
+    chrome.storage.local.get(["user_id"]).then((result) => {
+
+        fetch('https://663b-130-0-219-137.ngrok-free.app/check_tasks', {
+        method: 'POST',
+        body: JSON.stringify({
+                user_id : result['user_id'].toString(),
+            })
+        }).then(function(response) {
+            // Стоит проверить код ответа.
+            if (!response.ok) {
+                // Сервер вернул код ответа за границами диапазона [200, 299]
+                return Promise.reject(new Error(
+                    'Response failed: ' + response.status + ' (' + response.statusText + ')'
+                ));
+            }
+        
+            // Далее будем использовать только JSON из тела ответа.
+            return response.json();
+        }).then(function(data) {
+            check_response(data.task);
+        }).catch(function(error) {
+            console.log(error);
+        });
+        
+    });
+}
+
+function check_response(received_response){
+    // если в ответ че то приходит - посылаю уведомление
+    let myNotificationID = null;
+    if(received_response){
+        var options = {
+            title: received_response.header.toString(),
+            message: received_response.text.toString(),
+            iconUrl: "icons/128.png",
+            buttons: [
+                { title: 'Выполнить' },
+                { title: 'Отклонить' }
+              ],
+            type: "basic",
+          };
+          chrome.notifications.create("", options, function(id) {myNotificationID = id;});
+
+            chrome.notifications.onButtonClicked.addListener(function(notifId, btnIdx) {
+            if (notifId === myNotificationID) {
+                if (btnIdx === 0) {
+                    // выполнить
+                    fetch('https://663b-130-0-219-137.ngrok-free.app/', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                            user_id : result['user_id'].toString(),
+                        })
+                    }).then(function(response) {
+                        // Стоит проверить код ответа.
+                        if (!response.ok) {
+                            // Сервер вернул код ответа за границами диапазона [200, 299]
+                            return Promise.reject(new Error(
+                                'Response failed: ' + response.status + ' (' + response.statusText + ')'
+                            ));
+                        }
+                    
+                        // Далее будем использовать только JSON из тела ответа.
+                        return response.json();
+                    }).then(function(data) {
+                        check_response(data.task);
+                    }).catch(function(error) {
+                        console.log(error);
+                    });
+
+                    chrome.notifications.clear(myNotificationID);
+                } else if (btnIdx === 1) {
+                    // отклонить
+                    fetch('https://663b-130-0-219-137.ngrok-free.app/', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                            user_id : result['user_id'].toString(),
+                        })
+                    }).then(function(response) {
+                        // Стоит проверить код ответа.
+                        if (!response.ok) {
+                            // Сервер вернул код ответа за границами диапазона [200, 299]
+                            return Promise.reject(new Error(
+                                'Response failed: ' + response.status + ' (' + response.statusText + ')'
+                            ));
+                        }
+                    
+                        // Далее будем использовать только JSON из тела ответа.
+                        return response.json();
+                    }).then(function(data) {
+                        check_response(data.task);
+                    }).catch(function(error) {
+                        console.log(error);
+                    });
+
+                    chrome.notifications.clear(myNotificationID);
+                }
+            }
+        });
+    }
+
+    else{
+        console.log("Ошибка");
+    }
+}
--- src/view/popup.vue
+++ src/view/popup.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="main__div">
+
     <!-- Логин -->
-    <template v-if="login_status == false">
+    <template v-if="login_status == false && login == true">
       <div class="login">
-        <input type="email" placeholder="Email" v-model="login_text">
-        <input type="password" placeholder="Password" v-model="password_text">
-        <button @click="login">Войти</button>
+        <input type="email" placeholder="Email" v-model="login_email">
+        <input type="password" placeholder="Password" v-model="login_password">
+        <button @click="login_try">Войти</button>
+        <button @click="register_show">Зарегистрироваться</button>
       </div>
     </template>
 
-    <!-- Основная формочка -->
-    <template v-if="login_status == true">
+    <!-- Регистрация -->
+    <template v-if="login_status == false && register == true">
+      <div class="login">
+        <input type="email" placeholder="Email" v-model="register_email">
+        <input type="password" placeholder="Password" v-model="register_password">
+        <button @click="register_func">Регистрация</button>
+        <button @click="login_show">Назад</button>
+      </div>
+    </template>
+
+    <!-- Формочка добавления задачи -->
+    <template v-if="login_status == true && add_task == true">
+      <button class="login__back" @click="list_task_show">Назад</button>
       <div class="calendar__main__div">
         <div class="calendar__div">
           <DatePicker v-model="date" mode="dateTime" :min-date='new Date()' is24hr/>
@@ -18,10 +31,36 @@
 
         <div class="add__task">
           <textarea cols="29" rows="8" name="user_task" class="user__task" v-model="user_task"></textarea>
-          {{ test }}
           <button @click="create_task" class="btn__task">Создать</button>
         </div>
       </div>
+    </template>
+
+    <!-- Список задач (главная) -->
+    <template v-if="login_status == true && list_task == true ">
+      <div class="main__buttons">
+        <button class="main__btn" @click="logout_func">Выйти</button>
+        <div>
+          <button class="main__btn" @click="add_task_show">Добавить задачу</button>
+          <button class="main__btn" @click="statistic_show">Статистика</button>
+        </div>
+      </div>
+      <div class="list__task__main">
+        <template v-for="(task, index) in received_tasks.tasks" :key="index">
+          <div class="task__div">
+            <p>{{ task.header }}</p>
+            <p>{{ task.text }}</p>
+            <p>{{ task.start_time }}</p>
+            <P>{{ task.status }}</P>
+          </div>
+        </template>
+      </div>
+    </template>
+
+    <!-- Статистика -->
+    <template v-if="login_status == true && statistics == true ">
+      <button class="login__back" @click="list_task_show">Назад</button>
+      <p>Статистика</p>
     </template>
   </div>
 </template>
@@ -38,44 +77,149 @@ export default {
   name: 'popupView',
   data () {
     return {
+      // переменные для навигации
       login_status: false,
-      login_text: "",
-      password_text: "",
-      user_id: "",
-      task_list: false,
+      add_task: false,
+      list_task: false,
+      statistics: false,
+      register: false,
+      login: false,
 
+      // переменные для регистрации
+      register_email: "",
+      register_password: "",
+
+      // переменные для логина
+      login_email: "",
+      login_password: "",
+      user_id: "",
+
+      // переменные для создания задачи
       date: new Date(),
       user_task: "",
+      answer: "",
+      answer_error: "",
       test: "",
+
+      // переменные для списка задач
+      received_tasks: [],
+      tasks_error: "",
     }
   },
 
   mounted() {
       // при открытии расширения
-      this.login();
-      
+      this.login_func();
   },
 
   methods: {
-    // здесь логика логина. По хорошему сервак просто должен вернуть true или false
-    login(){
-      // пытаюсь получить логин и пароль с локального хранилища
-      // в будущем там можно хранить токены
-      let login = localStorage.getItem('login');
-      let password = localStorage.getItem('password');
-      this.user_id = localStorage.getItem('user_id');
-
-      if(login && password){
-        if(this.login_text == login && this.password_text == password){
-          this.login_status = true;
-        }
+    // здесь проверяю залогинен ли чувак
+    // получается у него должен быть user_id либо в localStorage, либо в глобальной пременной user_id
+    // если ни того, ни другого, то показываю ему окно логина
+    login_func(){
+      if(this.user_id != "" && this.user_id){
+          this.list_task_show();
       }
       else{
-        localStorage.setItem('login', "admin");
-        localStorage.setItem('password', "123");
-        localStorage.setItem('user_id', "1");
+        chrome.storage.local.get(["user_id"]).then((result) => {
+          if(result['user_id']){
+            this.user_id = result['user_id'].toString();
+            this.list_task_show();
+          }
+          else{
+            this.login_show();
+          }
+        });
       }
+    },
+
+    // здесь чел пытается залогиниться
+    login_try(){
+      let article = { email: this.login_email,
+                      password: this.login_password,
+                    };
+      axios.post("https://663b-130-0-219-137.ngrok-free.app/login_user", article)
+        .then(response => {this.user_id = response.data.user_id; this.save_user();})
+        .catch(error => { console.log(error.message); });
+    },
+
+
+
+    // регистрация
+    register_func(){
+      // при регистрации я посылю ему post запрос
+      let article = { email: this.register_email,
+                      password: this.register_password,
+                    };
+      axios.post("https://663b-130-0-219-137.ngrok-free.app/create_user", article)
+        .then(response => {this.user_id = response.data.user_id; this.save_user();})
+        .catch(error => { console.log(error.message); });
+    },
+
+    save_user(){
+      chrome.storage.local.set({ user_id: this.user_id.toString() }).then(() => {
+        console.log("Добавлен user_id");
+      });
+
+      this.login_func();
+    },
+
+    logout_func(){
+      this.user_id = "";
+      chrome.storage.local.clear();
+      this.login_show();
+    },
+
+    login_show(){
+      this.login_status = false;
+      this.list_task = false;
+      this.add_task = false;
+      this.statistics = false;
+      this.register = false;
+      this.login = true;
+    },
+
+    add_task_show(){
       this.login_status = true;
+      this.list_task = false;
+      this.add_task = true;
+      this.statistics = false;
+      this.register = false;
+      this.login = false;
+    },
+
+    statistic_show(){
+      this.login_status = true;
+      this.list_task = false;
+      this.add_task = false;
+      this.statistics = true;
+      this.register = false;
+      this.login = false;
+    },
+
+    register_show(){
+      this.login_status = false;
+      this.list_task = true;
+      this.add_task = false;
+      this.statistics = false;
+      this.register = true;
+      this.login = false;
+    },
+
+    list_task_show(){
+      this.login_status = true;
+      this.list_task = true;
+      this.add_task = false;
+      this.statistics = false;
+      this.register = false;
+      this.login = false;
+
+      // здесь я получаю список задач
+      let article = { user_id: this.user_id,
+                    };
+      axios.post("https://663b-130-0-219-137.ngrok-free.app/get_user_tasks", article)
+        .then(response => {this.received_tasks = response.data;})
+        .catch(error => { console.log(error.message); });
     },
 
     // создать задачу
@@ -97,19 +241,13 @@ export default {
       let task_text = this.user_task;
 
       // post запрос бэку
-      let info = "";
-      let error_msg = "";
       let article = { user_id: this.user_id,
                         header: task_text,
                         start_time: task_date,
                       };
-      axios.post("http://192.168.142.50:8000/create_task", article)
-        .then(response => this.test = response.data)
-        .catch(error => {
-          this.test = error.message;     
-      });
-    
-
+      axios.post("https://663b-130-0-219-137.ngrok-free.app/create_task", article)
+        .then(response => {this.test = response.data; this.list_task_show()})
+        .catch(error => { this.test = error.message; this.list_task_show()});
     },
   },
 }
@@ -155,7 +293,7 @@ body{
 .calendar__div{
   display: flex;
   justify-content: center;
-  margin-top: 30px;
+  margin-top: 10px;
   flex-direction: column;
   align-items: center;
 }
@@ -176,7 +314,7 @@ body{
 .btn__task{
   margin-left: 7px;
   margin-bottom: 7px;
-  background-color: rgb(239, 239, 254);
+  background-color: rgb(218, 218, 255);
   border: none;
   border-radius: 0.5rem;
   cursor: pointer;
@@ -184,5 +322,55 @@ body{
 
 .btn__task:hover{
   background-color: rgb(194, 194, 255);
+}
+
+.login__back{
+  margin-top: 10px;
+  margin-left: 10px;
+  margin-left: 7px;
+  margin-bottom: 7px;
+  background-color: rgb(207, 255, 185);
+  border: none;
+  border-radius: 0.5rem;
+  cursor: pointer;
+}
+
+.login__back:hover{
+  background-color: rgb(152, 255, 159);
+}
+
+.main__buttons{
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+
+.main__btn{
+  background-color: rgb(207, 255, 185);
+  border: none;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  margin: 5px;
+}
+
+.main__btn:hover{
+  background-color: rgb(152, 255, 159);
+}
+
+.list__task__main{
+  height: 100%;
+  margin-bottom: 10px;
+  margin-left: 5px;
+  margin-right: 5px;
+  background-color: white;
+  border-radius: 0.5rem;
+  padding: 5px;
+}
+
+.task__div{
+  background-color: rgb(240, 240, 240);
+  border-radius: 0.5rem;
+  margin: 5px;
+  padding: 5px;
 }
 </style>
